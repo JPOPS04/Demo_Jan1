0afa6a7e5735206e052fe7115a6929531fd3c946
JPOPS04 <122531664+JPOPS04@users.noreply.github.com>
2023-01-13 14:33:06 +0000

    Update builtScript.sh

--- .github/workflows/builtScript.sh
+++ .github/workflows/builtScript.sh
@@ -2,15 +2,16 @@
 
 on: [push]
 
-jobs:
+jobs1:
   actions_demo:
     runs-on: ubuntu-latest
     steps:
-    - run: touch newfile2.txt
-    - run: ls
-    - run: touch deltefile.txt
-    - run: ls
-    - run: rm deltefile.txt
-    - run: echo "newfile2" >> README.md
-    - run: chmod +x builtScript.sh
+     touch newfile2.txt
+     touch deltefile.txt
+     rm deltefile.txt
+
+jobs2:
+  actions2'_demo:
+     echo "newfile2" >> README.md
+     chmod +x builtScript.sh
     
